0cdfbd14cfe80b7ab9aa6080bed086a36d7ae176
karolpw <karol.piotr.wolak@gmail.com>
2026-03-11 12:50:06 +0100

    athorization for login and registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react'
 import BtnGet from './components/get-button'
-import FormPost from './components/post-form'
-import LoginForm from './components/login-form'
+import AuthForm from './components/auth-form'
 
 import './App.css'
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'))
 
+  const handleLogout = () =>{
+    localStorage.removeItem('token');
+    setToken(null)
+  }
+
   if (!token){
-    return <LoginForm onLogin={setToken}/>
+    return <AuthForm onLogin={setToken}/>
   }
 
   return (
     <>
       <div>
+        <button onClick={handleLogout}>Wyloguj się</button>
         <BtnGet/>
       </div>
     </>
